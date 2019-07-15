f29923226f627c2d5f2b6dfe388c7968433b12c4
chen <1065089607@qq.com>
2019-07-15 15:40:52 +0800

    xiugai

--- jiuyi_feign_robot/src/main/java/com/xiaoqiao/controller/robot/RobotController.java
+++ jiuyi_feign_robot/src/main/java/com/xiaoqiao/controller/robot/RobotController.java
@@ -16,8 +16,24 @@ public class RobotController {
     private RobotService robotService;
 
     @RequestMapping("inputByRobot")
-    private List<Drug> inputByRobot(){
-        return robotService.inputByRobot();
+    private List<Drug> inputByRobot(util.Params params){
+        //设置症状code
+        params.setSymptomCode("");
+        //设置症状code
+        params.setDrugCode(1);
+        //设置药店code
+        params.setDrugstoreCode(1);
+        //设置健康档案code
+        params.setHealthFileCode(1);
+        ////联合分类TYPE
+        params.setLhtype(1);
+        // 0:不是联合用药,1:联合用药
+        //params.setType();
+        //是否为医保 0：否，1：是
+        params.setYiBao(0);
+        //中西 1:中药,2:西药
+        params.setZhongXi(1);
+        return robotService.inputByRobot(params);
     }
 
 }
